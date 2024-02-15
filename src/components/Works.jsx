8498ee9644dadbbd57e191556ef12f2454ba3967
Thomas Stewart <thomasstewart@Thomass-MacBook-Pro-2.local>
2024-02-15 11:51:12 -0700

    fixed resume

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,13 +9,10 @@ const Works = () => {
     <div style={{ textAlign: 'center', border: '1px solid #ccc', padding: '20px', borderRadius: '5px', maxWidth: '600px', margin: '0 auto' }}>
       <h2 className={`${styles.sectionHeadText} justify-center`}>Resume</h2>
       <a href={ResumePDF} target="_blank" rel="noopener noreferrer" style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
-        <div style={{ position: 'relative' }}>
-          <img src={ResumeImage} alt="Resume Thumbnail" style={{ width: '100%', borderRadius: '5px' }} />
-          <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'rgba(255, 255, 255, 0.7)', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', fontSize: '24px' }}>Resume</div>
-        </div>
+        <img src={ResumeImage} alt="Resume Thumbnail" style={{ width: '100%', borderRadius: '5px' }} />
       </a>
     </div>
   );
 }
 
-export default SectionWrapper(Works,"work");
+export default SectionWrapper(Works, "work");
