0ac63cb4c52eb18deef60c724516f3cea67472e8
Thomas Stewart <thomaslstewart1@gmail.com>
2024-02-16 18:03:23 -0700

    added text

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
-import TableVideo from '../assets/Table.mp4'; // Import the video file outside of the component
-import { styles } from '../styles';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import ReactPlayer from "react-player";
+import TableVideo from "../assets/Table.mp4"; // Import the video file outside of the component
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "../utils/motion";
 
 function Table() {
   return (
-    <div className="containerVid">
-      <h1 className={styles.sectionHeadText}>
-        Proof of Concept of Automatic Drink Bar.
-      </h1>
-      <h2 className={styles.sectionSubText}>
-        Some portions have been sped for demonstration
-      </h2>
+    <div>
+      <div className="containerVid">
+        <h1 className={styles.sectionHeadText}>
+          Proof of Concept of Automatic Drink Bar.
+        </h1>
+        <h2 className={styles.sectionSubText}>
+          Some portions have been sped for demonstration
+        </h2>
         <div className="video-wrapper">
           <ReactPlayer
             url={TableVideo} // Use the imported variable directly
@@ -21,6 +24,20 @@ function Table() {
             height="450px"
           />
         </div>
+      </div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl justify-center"
+      >
+        Currently, the project is on pause as I'm working on rebuilding the
+        table with a stronger and more practical design. Instead of using slow,
+        food-safe pumps, I plan to switch to ones commonly found in drink
+        dispensers for better performance. The dispenser nozzle is controlled by
+        a servo motor. The movement of the cup is being done by a linear
+        actuator but a quiter, quicker solution is in the works. All this is
+        managed by motor controllers and an Arduino microcontroller, with the
+        software for the table being written in C++.{" "}
+      </motion.p>
     </div>
   );
 }
