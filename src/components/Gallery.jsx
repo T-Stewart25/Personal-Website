c7d8a9a7e5a96adde6b140cd4726e1c79f1eb343
Thomas Stewart <thomaslstewart1@gmail.com>
2024-02-19 15:05:06 -0700

    added arrows and shows only one image at a time

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { styles } from '../styles';
 import { SectionWrapper } from '../hoc';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 import One from '../assets/Gallery/One.png';
 import Two from '../assets/Gallery/Two.png';
@@ -16,7 +17,7 @@ import Eleven from '../assets/Gallery/Eleven.png';
 import Twelve from '../assets/Gallery/Twelve.png';
 
 const ImageGallery = () => {
-  const [images] = useState([
+  const images = [
     { src: Six, srcSet: Six, alt: 'Six' },
     { src: Twelve, srcSet: Twelve, alt: 'Twelve' },
     { src: Four, srcSet: Four, alt: 'Four' },
@@ -29,7 +30,7 @@ const ImageGallery = () => {
     { src: Eleven, srcSet: Eleven, alt: 'Eleven' },
     { src: Nine, srcSet: Nine, alt: 'Nine' },
     { src: One, srcSet: One, alt: 'One' },
-  ]);
+  ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [lightboxOpen, setLightboxOpen] = useState(false);
@@ -43,10 +44,14 @@ const ImageGallery = () => {
     setLightboxOpen(false);
   };
 
-  const handleOutsideClick = (event) => {
-    if (event.target === event.currentTarget) {
-      closeLightbox();
-    }
+  const handlePreviousClick = () => {
+    const prevIndex = currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1;
+    setCurrentImageIndex(prevIndex);
+  };
+
+  const handleNextClick = () => {
+    const nextIndex = (currentImageIndex + 1) % images.length;
+    setCurrentImageIndex(nextIndex);
   };
 
   return (
@@ -54,49 +59,17 @@ const ImageGallery = () => {
       <div className='flex flex-row flex-wrap justify-center gap-10'>
         <h2 className={`${styles.sectionHeadText} justify-center`}>Life-Style.</h2>
       </div>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '20px' }}>
-        {images.map((image, index) => (
-          <div key={index} onClick={() => openLightbox(index)} style={{ border: '3px solid #000', padding: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <img src={image.src} srcSet={image.srcSet} alt={image.alt} />
-          </div>
-        ))}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh', overflowX: 'scroll' }}>
+      <IoIosArrowBack onClick={handlePreviousClick} style={{ cursor: 'pointer' }} />
+        <div key={images[currentImageIndex].src} onClick={() => openLightbox(currentImageIndex)} style={{ border: '3px solid #000', padding: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={images[currentImageIndex].src} srcSet={images[currentImageIndex].srcSet} alt={images[currentImageIndex].alt} style={{ maxHeight: '70vh' }} />
+        </div>
+        <IoIosArrowForward onClick={handleNextClick} style={{ cursor: 'pointer' }} />
       </div>
       {lightboxOpen && (
-        <div
-          className="lightbox-backdrop"
-          style={{
-            zIndex: 1100,
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: 'rgba(0, 0, 0, 0.8)',
-          }}
-          onClick={handleOutsideClick}
-        >
-          <div
-            className="lightbox-content"
-            style={{
-              zIndex: 1101,
-              position: 'fixed',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              maxWidth: 'calc(100% - 200px)',
-              maxHeight: 'calc(100% - 200px)',
-              overflow: 'auto',
-              padding: '5px',
-              backgroundColor: '#fff',
-              boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
-            }}
-          >
-            <img
-              src={images[currentImageIndex].src}
-              srcSet={images[currentImageIndex].srcSet}
-              alt={images[currentImageIndex].alt}
-              style={{ width: '100%', height: 'auto' }}
-            />
+        <div className="lightbox-backdrop" style={{ zIndex: 1100, position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0, 0, 0, 0.8)' }} onClick={closeLightbox}>
+          <div className="lightbox-content" style={{ zIndex: 1101, position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: 'calc(100% - 200px)', maxHeight: 'calc(100% - 200px)', overflow: 'auto', padding: '5px', backgroundColor: '#fff', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)' }}>
+            <img src={images[currentImageIndex].src} srcSet={images[currentImageIndex].srcSet} alt={images[currentImageIndex].alt} style={{ width: '100%', height: 'auto' }} />
           </div>
         </div>
       )}
